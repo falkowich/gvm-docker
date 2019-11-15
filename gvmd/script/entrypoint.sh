2784cdb339e0e4c7c6b0f500c064e168e361c346
Falk <andreas.alk@nykoping.se>
2019-11-15 16:16:41 +0100

    first try working monolithic build

--- gvmd/script/entrypoint.sh
+++ gvmd/script/entrypoint.sh
@@ -12,6 +12,23 @@ do
   /opt/gvm/bin/gvm-manage-certs -af
 done
 
+  service postgresql restart 
+
+echo "===> Checking if db is ready"
+if psql gvmd -c '\q' 2>&1; then
+   echo "database is ready"
+else
+  echo "Creating database gvmd and settings"
+  su postgres sh -c "createuser -DRS gvm" ;\
+  su postgres sh -c "createdb -O gvm gvmd" ;\
+  su postgres sh -c "psql -d gvmd"   << EOSQL
+      create role dba with superuser noinherit;
+      grant dba to gvm;
+      CREATE EXTENSION "uuid-ossp";
+EOSQL
+fi
+
+
 
 echo "===> Waiting to get psql up and running"
 until ls -alh /var/run/postgresql/.s.PGSQL.5432
@@ -32,6 +49,13 @@ fi
 # Start GVM stuffs
 echo "---> Starting GVMD"
 gvmd --listen=0.0.0.0 --port=9391 --osp-vt-update=/opt/gvm/var/run/ospd.sock
+
+echo "---> Starting ospd-openvas"
+/opt/gvm/bin/ospd-scanner/bin/python /opt/gvm/bin/ospd-scanner/bin/ospd-openvas \
+                                      --pid-file /opt/gvm/var/run/ospd-openvas.pid \
+                                      --unix-socket=/opt/gvm/var/run/ospd.sock \
+                                      --log-file /opt/gvm/var/log/gvm/ospd-scanner.log
+
 
 # WHATTODOWITTHIS?
 if [ -z "$BUILD" ]; then
